4d393fb91f29be3964a5e96831e83c02f4f4b625
jjuarezg20 <20235076@esen.edu.sv>
2026-02-08 17:10:29 -0600

    Change base path in Vite configuration

--- Guia3_20235076/Guía_3/vite.config.js
+++ Guia3_20235076/Guía_3/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Guia3_20235076/Gu%C3%ADa_3/',
+  base: './',
   plugins: [react()],
 })
 
